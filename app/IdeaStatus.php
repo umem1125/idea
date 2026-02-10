f5e663f22955c137ee9cf98d795f10b5df1874a8
umem1125 <chaerulumam1125@gmail.com>
2026-02-10 09:16:53 +0700

    setup models, relations, enums

---
+++ app/IdeaStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+enum IdeaStatus: string
+{
+    case PENDING = 'pending';
+    case IN_PROGRESS = 'in_progress';
+    case COMPLETED = 'completed';
+
+    // method
+    public function label(): string
+    {
+        return match ($this) {
+            self::PENDING => 'Pending',
+            self::IN_PROGRESS => 'In Progress',
+            self::COMPLETED => 'Completed',
+        };
+    }
+}
---
+++ app/Models/Idea.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\IdeaStatus;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Idea extends Model
+{
+    /** @use HasFactory<\Database\Factories\IdeaFactory> */
+    use HasFactory;
+
+    protected $casts = [
+        'links' => AsArrayObject::class,
+        'status' => IdeaStatus::class
+    ];
+
+    protected $attributes = [
+        'status' => IdeaStatus::PENDING
+    ];
+
+    // relations belongs to user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // relations the ideas has many steps
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class);
+    }
+}
