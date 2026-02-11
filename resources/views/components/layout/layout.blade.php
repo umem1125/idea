db10a2f04cb5274dcc62dc9b812fe779f99d8fbe
umem1125 <chaerulumam1125@gmail.com>
2026-02-11 07:23:13 +0700

    install alpine js for flash message

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-background text-foreground">
@@ -17,6 +17,13 @@
     <main class="max-w-7xl mx-auto px-6">
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.opacity.duration.600m
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+            {{ $value }}
+        </div>
+    @endsession
 </body>
 
 </html>
