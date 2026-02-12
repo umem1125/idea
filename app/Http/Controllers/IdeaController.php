d0a01cc1f4207f10ed9a4ac60ffb8bfeaed156f4
umem1125 <chaerulumam1125@gmail.com>
2026-02-12 17:27:50 +0700

    create a gate policy for authorization

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -10,6 +10,7 @@ use App\IdeaStatus;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class IdeaController extends Controller
 {
@@ -58,6 +59,8 @@ class IdeaController extends Controller
      */
     public function show(Idea $idea)
     {
+        Gate::authorize('workWith', $idea);
+
         return view('idea.show', [
             'idea' => $idea
         ]);
@@ -68,7 +71,7 @@ class IdeaController extends Controller
      */
     public function edit(Idea $idea)
     {
-        //
+        Gate::authorize('workWith', $idea);
     }
 
     /**
@@ -76,7 +79,7 @@ class IdeaController extends Controller
      */
     public function update(UpdateIdeaRequest $request, Idea $idea)
     {
-        //
+        Gate::authorize('workWith', $idea);
     }
 
     /**
@@ -84,6 +87,8 @@ class IdeaController extends Controller
      */
     public function destroy(Idea $idea)
     {
+        Gate::authorize('workWith', $idea);
+
         $idea->delete();
 
         return to_route('idea.index');
--- tests/Browser/CreateIdeaTest.php
+++ tests/Browser/CreateIdeaTest.php
@@ -14,14 +14,20 @@ it('creates the new idea', function () {
         ->click('@submit-new-link-button')
         ->fill('@new-link', 'https://laracasts.com')
         ->click('@submit-new-link-button')
+        ->fill('@new-step', 'Do a thing')
+        ->click('@submit-new-step-button')
+        ->fill('@new-step', 'Do another thing')
+        ->click('@submit-new-step-button')
         ->click('Create')
         ->assertPathIs('/ideas');
 
 
-    expect($user->ideas()->first())->toMatchArray([
+    expect($idea = $user->ideas()->first())->toMatchArray([
         'title' => 'Example Title',
         'status' => 'completed',
         'description' => 'Example Test Description',
         'links' => ['https://laravel.com', 'https://laracasts.com']
     ]);
+
+    expect($idea->steps)->toHaveCount(2);
 });
